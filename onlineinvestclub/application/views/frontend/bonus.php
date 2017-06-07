535425c866c97b2a7a3d7fd53d8021e4a3da9689
Tejal <amit.ambekar979@gmail.com>
2017-06-07 10:10:39 +0530

    changes

---
+++ onlineinvestclub/application/views/frontend/bonus.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<div id="wrapper">
+  <?php $this->view('frontend/includes/header-1'); ?>
+  <div class="textsec">
+    <div class="container">
+  <h1>Bonus</h1>
+  
+  
+  </div>
+  </div>
+  <?php $this->view('frontend/includes/footer'); ?>
+</div>
+<script>
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+});
+</script>
+</body></html>
